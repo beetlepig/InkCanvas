51cd75f4641e49c128fb1b918558ae70207cfdcd
karlinux <sky_karlos@hotmail.com>
2018-11-05 03:48:45 -0500

    cancelar sala

--- src/containers/WaitScreen.js
+++ src/containers/WaitScreen.js
@@ -5,7 +5,7 @@
 
 import React, { Component } from 'react';
 import {View} from 'react-native';
-import {Text} from "react-native-elements";
+import {Button, Text} from "react-native-elements";
 import {stores} from "../stores";
 
 type Props = {
@@ -21,10 +21,16 @@ export default class WaitScreen extends Component<Props, {}> {
         stores.firestore.listenActiveDocument();
     }
 
+    cancelarSala() {
+        stores.firestore.removeActiveDocument();
+        stores.ui.setCurrentScreen('MAIN');
+    }
+
         render() {
             return(
             <View>
                 <Text>Esperando</Text>
+                <Button title={'Cancelar'} onPress={() => this.cancelarSala()}/>
             </View>
             )
         }
